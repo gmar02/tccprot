552be76ab5c5f007ac5c3fc44b0e26bbaf449df5
gmar02 <gmarinho2002@hotmail.com>
2025-10-27 18:03:26 -0400

    altera codificação da resposta e desativa log do servidor

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, make_response
+import logging as pylogging
 
 app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
+
+# ROTAS #######################################################################
 
 @app.route('/teste', methods=['GET'])
 def teste():
-    return jsonify({"mensagem": "Teste concluído com sucesso!"}), 200
+    resposta = make_response(jsonify({"mensagem": "Teste concluído com sucesso!"}))
+    resposta.headers["Content-Type"] = "application/json; charset=utf-8"
+    return resposta
+
+###############################################################################
 
 if __name__ == '__main__':
-    print("Iniciando API...")
-    app.run()
+    # desativando logging do servidor de desenvolvimento
+    log = pylogging.getLogger("werkzeug")
+    log.setLevel(pylogging.ERROR)
+
+    print("Iniciando API em servidor de desenvolvimento...")
+    print("API iniciada em http://0.0.0.0:5000")
+
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
